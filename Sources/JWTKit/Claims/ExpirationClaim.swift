072ab619011ff08ff75df31c982ac3b2afb80727
Geert Berkers <geertberkers@hotmail.com>
2021-06-17 11:18:05 +0200

    Added timeformatter + correct logging

--- Sources/JWTKit/Claims/ExpirationClaim.swift
+++ Sources/JWTKit/Claims/ExpirationClaim.swift
@@ -10,19 +10,13 @@ import Foundation
 /// containing a NumericDate value.  Use of this claim is OPTIONAL.
 public struct ExpirationClaim: JWTUnixEpochClaim, Equatable {
     
-    var ISO8601DateFormatter: DateFormatter = {
+    public var ISO8601DateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat =  "yyyy-MM-dd'T'HH:mm:ss.SSSSZZZ"
-        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.timeZone = TimeZone(abbreviation: "GMT+02")
         return formatter
     }()
-    
-    let addHours: Double  = 60 * 60 * 2 // (60 seconds * 60 minutes * 2 hours)
-        
-//    var addHours: Double {
-//        60 * 60 * 2 // (60 seconds * 60 minutes * 2 hours)
-//    }
     
     /// See `JWTClaim`.
     public var value: Date
@@ -30,34 +24,25 @@ public struct ExpirationClaim: JWTUnixEpochClaim, Equatable {
     /// See `JWTClaim`.
     public init(value: Date) {
         self.value = value
-
-        let addedTime = value.addingTimeInterval(self.addHours)
-        let initDateString = ISO8601DateFormatter.string(from: value)
-        let addedDateString = ISO8601DateFormatter.string(from: addedTime)
-
-        Log.debug("Self date: \(initDateString))")
-        Log.debug("Date + 2H: \(addedDateString)")
     }
 
     /// Throws an error if the claim's date is later than current date.
     public func verifyNotExpired(currentDate: Date = .init()) throws {
-        let initDateString = ISO8601DateFormatter.string(from: value)
+        let initDateString = ISO8601DateFormatter.string(from: self.value)
         let currentDateString = ISO8601DateFormatter.string(from: currentDate)
 
-        Log.debug("Self date: \(initDateString))")
+        Log.debug("Date:         \(initDateString)")
         Log.debug("Current Date: \(currentDateString)")
-//        let updatedCurrentDate = currentDate.addingTimeInterval(-addHours)
-//
-//        let currentDateString = ISO8601DateFormatter.string(from: currentDate)
-//        let updatedDateString = ISO8601DateFormatter.string(from: updatedCurrentDate)
-//
-//        Log.debug("Current date: \(currentDateString)")
-//        Log.debug("Updated date (-2H): \(updatedDateString)")
-
+        
         switch self.value.compare(currentDate) {
-        case .orderedAscending, .orderedSame:
+        case .orderedAscending:
+            Log.debug("Value ascending!")
+            throw JWTError.claimVerificationFailure(name: "exp", reason: "expired")
+        case .orderedSame:
+            Log.debug("Value ordered same!")
             throw JWTError.claimVerificationFailure(name: "exp", reason: "expired")
         case .orderedDescending:
+            Log.debug("Value descending!")
             break
         }
     }
